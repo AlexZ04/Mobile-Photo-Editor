0b83a5719fde789905cf307ae3c8ac51334782b8
pineapplleboy <pineapplleboy@gmail.com>
2024-04-23 16:03:49 +0700

    feat: add picture pick screen

--- app/src/main/java/com/example/photoeditor/MainActivity.kt
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.photoeditor
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_CODE = 1
+    private lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +30,42 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        button = findViewById(R.id.upload)
+
+        button.setOnClickListener{
+
+//            if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.S
+//                && ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED ||
+//                android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S
+//                && ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_MEDIA_IMAGES) == PackageManager.PERMISSION_GRANTED){
+//
+//                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+//                startActivityForResult(intent, REQUEST_CODE)
+//            }
+//            else{
+//                if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S){
+//                    ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_MEDIA_IMAGES), REQUEST_CODE)
+//                }
+//                else{
+//                    ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE)
+//                }
+//            }
+
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==REQUEST_CODE && resultCode == RESULT_OK && data != null){
+            val uri: Uri? = data.data
+
+            val intent = Intent(this, EditorActivity::class.java).apply {
+                setData(uri)
+            }
+            startActivity(intent)
         }
     }
 }
