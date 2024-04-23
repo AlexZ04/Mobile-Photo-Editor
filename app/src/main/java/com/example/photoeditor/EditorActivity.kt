109da82f7db346f8cdbba4ffd71a21641b561979
BACHAEV007 <mv.bachaev@gmail.com>
2024-04-23 17:26:36 +0700

    feat: add ColorFilters class

--- app/src/main/java/com/example/photoeditor/EditorActivity.kt
+++ app/src/main/java/com/example/photoeditor/EditorActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.photoeditor.Filter.ColorFilters
 
 class EditorActivity : AppCompatActivity() {
 
@@ -36,5 +37,6 @@ class EditorActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+        ColorFilters(bitmap);
     }
 }
