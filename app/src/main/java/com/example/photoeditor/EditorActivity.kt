b144fe69d7207b91b730709cbdd9554138574627
AlexZ04 <alexz04tab2@gmail.com>
2024-04-24 10:02:47 +0700

    feat: add blackWhiteFilter and balanceWhite filter

--- app/src/main/java/com/example/photoeditor/EditorActivity.kt
+++ app/src/main/java/com/example/photoeditor/EditorActivity.kt
@@ -39,7 +39,7 @@ class EditorActivity : AppCompatActivity() {
         }
 
         val objectImage = ColorFilters(bitmap)
-        objectImage.negativeFilter()
+        objectImage.blackWhiteFilter()
         mainImage.setImageBitmap(objectImage.bitmap)
     }
 }
--- app/src/main/java/com/example/photoeditor/Filter/ColorFilters.kt
+++ app/src/main/java/com/example/photoeditor/Filter/ColorFilters.kt
@@ -2,20 +2,76 @@ package com.example.photoeditor.Filter
 
 import android.graphics.Bitmap
 import android.graphics.Color
+import kotlin.math.abs
 
 class ColorFilters(_bitmap: Bitmap) {
 //    val bitmap: Bitmap = _bitmap
     var bitmap = _bitmap.copy(Bitmap.Config.ARGB_8888, true)
     fun negativeFilter() {
-        println(this.bitmap.width)
 
         for (x in 0 until this.bitmap.getWidth()) {
             for (y in 0 until this.bitmap.getHeight()) {
-                var r = this.bitmap.getColor(x, y).red()
-                var g = this.bitmap.getColor(x, y).green()
-                var b = this.bitmap.getColor(x, y).blue()
+                val color = this.bitmap.getPixel(x, y)
+
+                val r = Color.red(color)
+                val g = Color.green(color)
+                val b = Color.blue(color)
 
                 this.bitmap.setPixel(x, y, Color.rgb(1 - r, 1 - g, 1 - b))
+            }
+        }
+    }
+
+    fun blackWhiteFilter() {
+
+        for (x in 0 until this.bitmap.getWidth()) {
+            for (y in 0 until this.bitmap.getHeight()) {
+                val color = this.bitmap.getPixel(x, y)
+
+                val r = Color.red(color)
+                val g = Color.green(color)
+                val b = Color.blue(color)
+
+                val newColor = (r + g + b) / 3
+
+                this.bitmap.setPixel(x, y, Color.rgb(newColor, newColor, newColor))
+            }
+        }
+    }
+
+    fun balanceWhite() { // как будто он не так должен работать
+
+        var r = 0
+        var g = 0
+        var b = 0
+
+        for (x in 0 until this.bitmap.getWidth()) {
+            for (y in 0 until this.bitmap.getHeight()) {
+                val color = this.bitmap.getPixel(x, y)
+
+                r += Color.red(color)
+                g += Color.green(color)
+                b += Color.blue(color)
+            }
+        }
+
+        r /= (this.bitmap.getWidth() * this.bitmap.getHeight())
+        g /= (this.bitmap.getWidth() * this.bitmap.getHeight())
+        b /= (this.bitmap.getWidth() * this.bitmap.getHeight())
+
+        println(r)
+
+        val newColor = (r + g + b) / 3
+
+        for (x in 0 until this.bitmap.getWidth()) {
+            for (y in 0 until this.bitmap.getHeight()) {
+                val color = this.bitmap.getPixel(x, y)
+
+                val newR = abs(Color.red(color) - newColor) / 2
+                val newG = abs(Color.green(color) - newColor) / 2
+                val newB = abs(Color.blue(color) - newColor) / 2
+
+                this.bitmap.setPixel(x, y, Color.rgb(newR, newG, newB))
             }
         }
     }
