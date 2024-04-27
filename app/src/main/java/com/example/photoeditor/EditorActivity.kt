1127c025253fa7e8c93e6f2f557bfbf46b79bd3a
pineapplleboy <pineapplleboy@gmail.com>
2024-04-27 13:11:54 +0700

    feat: add picture rotation

--- app/src/main/java/com/example/photoeditor/EditorActivity.kt
+++ app/src/main/java/com/example/photoeditor/EditorActivity.kt
@@ -1,21 +1,26 @@
 package com.example.photoeditor
 
 import android.content.Intent
+import android.media.ExifInterface
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.photoeditor.Filter.ColorFilters
+import com.example.photoeditor.Translate.Rotate
+
 
 class EditorActivity : AppCompatActivity() {
 
     private lateinit var mainImage: ImageView
     private lateinit var choosePickButton: Button
+    private lateinit var confirmButton: Button
+    private lateinit var angleValueText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +34,25 @@ class EditorActivity : AppCompatActivity() {
 
         mainImage = findViewById(R.id.mainImage)
         choosePickButton = findViewById(R.id.choosePickButton)
+        confirmButton = findViewById(R.id.confirmButton)
+        angleValueText = findViewById(R.id.angleInput)
 
-        val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, intent.data)
+        val uri: Uri = intent.data!!
+
+        var bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+
+        val objectImage = Rotate(bitmap)
+
+        val exif = ExifInterface(contentResolver.openInputStream(uri)!!)
+        val orientation: Int =
+            exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+
+        when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> bitmap = objectImage.rotate(90.0)
+            ExifInterface.ORIENTATION_ROTATE_180 -> bitmap = objectImage.rotate(180.0)
+            ExifInterface.ORIENTATION_ROTATE_270 -> bitmap = objectImage.rotate(270.0)
+        }
+
         mainImage.setImageBitmap(bitmap)
 
         choosePickButton.setOnClickListener{
@@ -38,8 +60,9 @@ class EditorActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        val objectImage = ColorFilters(bitmap)
-        objectImage.negativeFilter()
-        mainImage.setImageBitmap(objectImage.bitmap)
+        confirmButton.setOnClickListener{
+
+            mainImage.setImageBitmap(objectImage.rotate(-angleValueText.text.toString().toDouble()))
+        }
     }
 }
---
+++ app/src/main/java/com/example/photoeditor/Translate/Rotate.kt
@@ -0,0 +1,63 @@
+package com.example.photoeditor.Translate
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import androidx.core.graphics.get
+import androidx.core.graphics.toColor
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sin
+
+class Rotate(_bitmap: Bitmap) {
+
+    var bitmap = _bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+
+    private fun getNewPos(x: Int, y: Int, angle: Double): IntArray {
+
+        val newX = (x * cos(angle) + y * sin(angle)).toInt()
+        val newY = (y * cos(angle) - x * sin(angle)).toInt()
+
+        return intArrayOf(newX, newY)
+    }
+
+    fun rotate(angle: Double) : Bitmap {
+
+        var minX = Int.MAX_VALUE
+        var minY = Int.MAX_VALUE
+        var maxX = Int.MIN_VALUE
+        var maxY = Int.MIN_VALUE
+
+        val angleRad = PI * angle / 180
+
+        for(i in 0..<bitmap.width * 2 step bitmap.width){
+            for(j in 0..<bitmap.height * 2 step bitmap.height){
+
+                val newX = (i * cos(angleRad) - j * sin(angleRad)).toInt()
+                val newY = (i * sin(angleRad) + j * cos(angleRad)).toInt()
+                minX = min(minX, newX)
+                minY = min(minY, newY)
+                maxX = max(maxX, newX)
+                maxY = max(maxY, newY)
+            }
+        }
+
+        val newBitmap = Bitmap.createBitmap(maxX - minX, maxY - minY, Bitmap.Config.ARGB_8888)
+        for (x in 0..<newBitmap.getWidth()) {
+            for (y in 0..<newBitmap.getHeight()) {
+
+                val(newX, newY) = getNewPos(x + minX, y + minY, angleRad)
+
+                val newColor = if(newX >= 0 && newX < this.bitmap.width
+                    && newY >= 0 && newY < this.bitmap.height) this.bitmap.getColor(newX, newY) else Color.BLACK.toColor()
+
+                newBitmap.setPixel(x, y, newColor.toArgb())
+            }
+        }
+
+        return newBitmap
+    }
+}
