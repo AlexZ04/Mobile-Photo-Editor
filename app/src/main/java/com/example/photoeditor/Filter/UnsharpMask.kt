b1d87900bab25f2a01e09935960a35878052285a
AlexZ04 <alexz04tab2@gmail.com>
2024-05-20 22:38:16 +0700

    feat: add unsharpMask file

---
+++ app/src/main/java/com/example/photoeditor/Filter/UnsharpMask.kt
@@ -0,0 +1,13 @@
+package com.example.photoeditor.Filter
+
+import android.graphics.Bitmap
+import android.graphics.Color
+import kotlin.math.abs
+
+class UnsharpMask(_bitmap: Bitmap) {
+    var bitmap = _bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+    fun unsharpMaskAlg() {
+
+    }
+}
