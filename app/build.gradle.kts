36ff911d6f3417ae728e6bea2a0ee4b95575358b
AlexZ04 <alexz04tab2@gmail.com>
2024-05-21 20:48:13 +0700

    fix smth

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "photoeditor"
 include(":app")
- 
